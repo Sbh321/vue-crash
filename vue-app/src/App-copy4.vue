bd7a49597ab1f6c2d37be4296681070878efe272
Subham <bhandarimaiya65@gmail.com>
2024-08-20 10:43:21 +0545

    onMounted and Fetching data

---
+++ vue-app/src/App-copy4.vue
@@ -0,0 +1,78 @@
+<!-- Composition API Short Form-->
+<!-- ref is like useState in react -->
+
+<script setup>
+import { onMounted, ref } from "vue";
+
+const name = ref("Subham Bhandari");
+const status = ref("free");
+const message = ref("Hello! Welcome");
+const tasks = ref(["brush", "college", "intern"]);
+const link = ref("https://google.com");
+const newTask = ref("Some Task");
+
+const toggleStatus = () => {
+  if (status.value === "busy") {
+    status.value = "free";
+  } else if (status.value === "free") {
+    status.value = "busy";
+  } else {
+    status.value = "haha";
+  }
+};
+
+const addTask = () => {
+  if (newTask.value.trim() !== "") {
+    tasks.value.push(newTask.value);
+    newTask.value = "";
+  }
+};
+
+const deleteTask = (index) => {
+  tasks.value.splice(index, 1);
+};
+
+onMounted(async () => {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+    const data = await response.json();
+    // console.log(data);
+    tasks.value = data.map((task) => task.title);
+  } catch (error) {
+    console.log(error);
+  }
+
+  setTimeout(() => {
+    message.value = "Hello! Welcome to Vue 3";
+  }, 2000);
+});
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+  <p v-if="status === 'free'">User is free</p>
+  <p v-else-if="status === 'busy'">User is busy</p>
+  <p v-else>User is inactive</p>
+
+  <form @submit.prevent="addTask">
+    <label for="newTask">Add Task</label>
+    <input type="text" id="newTask" name="newTask" v-model="newTask" />
+    <button type="submit">Submit</button>
+  </form>
+
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-if="tasks.length === 0">No Tasks</li>
+    <li v-for="(task, index) in tasks" :key="task">
+      <span>{{ task }}</span>
+      <button @click="deleteTask(index)">Delete</button>
+    </li>
+  </ul>
+
+  <!-- Attribute binding in vue -->
+  <!-- <a v-bind:href="link">Visit Google</a> -->
+  <a :href="link">Visit Google</a>
+  <br />
+  <!-- <button v-on:click="toggleStatus">Change Status</button> -->
+  <button @click="toggleStatus">Change Status</button>
+</template>
--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,63 +1,7 @@
-<!-- Composition API Short Form-->
-<!-- ref is like useState in react -->
-
-<script setup>
-import { ref } from "vue";
-
-const name = ref("Subham Bhandari");
-const status = ref("free");
-const message = ref("Hello! Welcome");
-const tasks = ref(["brush", "college", "intern"]);
-const link = ref("https://google.com");
-const newTask = ref("Some Task");
-
-const toggleStatus = () => {
-  if (status.value === "busy") {
-    status.value = "free";
-  } else if (status.value === "free") {
-    status.value = "busy";
-  } else {
-    status.value = "haha";
-  }
-};
-
-const addTask = () => {
-  if (newTask.value.trim() !== "") {
-    tasks.value.push(newTask.value);
-    newTask.value = "";
-  }
-};
-
-const deleteTask = (index) => {
-  tasks.value.splice(index, 1);
-};
+<script>
+import { ref, onMounted } from "vue";
 </script>
 
 <template>
-  <h1>{{ message }}</h1>
-  <p v-if="status === 'free'">User is free</p>
-  <p v-else-if="status === 'busy'">User is busy</p>
-  <p v-else>User is inactive</p>
-
-  <form @submit.prevent="addTask">
-    <label for="newTask">Add Task</label>
-    <input type="text" id="newTask" name="newTask" v-model="newTask" />
-    <button type="submit">Submit</button>
-  </form>
-
-  <h3>Tasks:</h3>
-  <ul>
-    <li v-if="tasks.length === 0">No Tasks</li>
-    <li v-for="(task, index) in tasks" :key="task">
-      <span>{{ task }}</span>
-      <button @click="deleteTask(index)">Delete</button>
-    </li>
-  </ul>
-
-  <!-- Attribute binding in vue -->
-  <!-- <a v-bind:href="link">Visit Google</a> -->
-  <a :href="link">Visit Google</a>
-  <br />
-  <!-- <button v-on:click="toggleStatus">Change Status</button> -->
-  <button @click="toggleStatus">Change Status</button>
+  <h1>Jobs</h1>
 </template>
